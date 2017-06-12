7a515f23e3d0ad5f6534a2f97f4d82f0459ca517
liwenqiu <liwenqiu@gmail.com>
2017-06-12 11:25:48 +0800

    Ch02 Completed

---
+++ classpath/entry.go
@@ -0,0 +1,28 @@
+package classpath
+
+import (
+	"os"
+	"strings"
+)
+
+const pathListSeparator = string(os.PathListSeparator)
+
+type Entry interface {
+	// pass java/lang/Object, return class file as []byte
+	readClass(className string) ([]byte, Entry, error)
+	String() string
+}
+
+func newEntry(path string) Entry {
+	if strings.Contains(path, pathListSeparator) {
+		return newCompositeEntry(path)
+	}
+	if strings.HasSuffix(path, "*") {
+		return newWildcardEntry(path)
+	}
+	if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".JAR") ||
+		strings.HasSuffix(path, ".zip") || strings.HasSuffix(path, ".ZIP") {
+		return newZipEntry(path)
+	}
+	return newDirEntry(path)
+}
