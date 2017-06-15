81b9ab39915dc71390158b64a0394ec449aeef35
liwenqiu <liwenqiu@gmail.com>
2017-06-15 14:48:49 +0800

    Ch08 Completed

--- rtda/heap/object.go
+++ rtda/heap/object.go
@@ -2,13 +2,13 @@ package heap
 
 type Object struct {
     class *Class
-	fields Slots
+	data  interface{}
 }
 
 func newObject(class *Class) *Object {
 	return &Object{
 		class:  class,
-		fields: newSlots(class.instanceSlotCount),
+		data: newSlots(class.instanceSlotCount),
 	}
 }
 
@@ -17,9 +17,21 @@ func (self *Object) Class() *Class {
 	return self.class
 }
 func (self *Object) Fields() Slots {
-	return self.fields
+	return self.data.(Slots)
 }
 
 func (self *Object) IsInstanceOf(class *Class) bool {
 	return class.isAssignableFrom(self.class)
+}
+
+// reflection
+func (self *Object) GetRefVar(name, descriptor string) *Object {
+	field := self.class.getField(name, descriptor, false)
+	slots := self.data.(Slots)
+	return slots.GetRef(field.slotId)
+}
+func (self *Object) SetRefVar(name, descriptor string, ref *Object) {
+	field := self.class.getField(name, descriptor, false)
+	slots := self.data.(Slots)
+	slots.SetRef(field.slotId, ref)
 }
